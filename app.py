77184b8e01a0c02793bc99ff235efb84972ac29e
Taylor Alfers <Taylor>
2019-10-27 07:42:29 -0600

    err handling

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, request, render_template, redirect
 # Create app var from Flask package
 app = Flask(__name__)
 
-# This packages allows for saving files
+# This packages allows for saving files to app dir
 import os
 
-# Set path to upload imgs
+# Set path to upload csv (path of current app dirnae)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Create get route, function to run on request
@@ -14,17 +14,21 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 def first_route():
     return render_template('upload.html')
 
-# Create post, get route, function to run on request
+# Create post route, function to run on request
 @app.route('/results', methods=['POST', 'GET'])
 def send_csv():
     if request.method == 'POST':
-        if request.files:
+        if request.files['file'].filename == '':
+            return render_template('upload.html')
+        else:
             target = os.path.join(APP_ROOT, 'static/uploads')
             file = request.files['file']
             filename = file.filename
             destination = "/".join([target, filename])
             file.save(destination)
             return render_template('results.html', download_file = filename)
+    else:
+        return render_template('upload.html')
 
 # This runs the server (provided by Flask)
 if __name__ == '__main__':
